e48c844e859ceaf3179438af5bda5fcac37a5193
Serega71RUS <serega71rus@gmail.com>
2021-09-30 17:26:20 +0300

    teacher answer task#1 012

--- js/script.js
+++ js/script.js
@@ -3,25 +3,22 @@
 //confirm
 //prompt
 //= == ===
-let numberOffilms = prompt("Сколько фильмов вы уже посмотрели?");
+const numberOffilms = prompt("Сколько фильмов вы уже посмотрели?");
+
 const personalMovieDB = {
   count: numberOffilms,
-  movies: {
-
-  },
-  actors: {
-
-  },
-  genres: [
-
-  ],
+  movies: {},
+  actors: {},
+  genres: [],
   privat: false
 };
 
-for(let i = 0; i < numberOffilms; i++){
-  let namemv = prompt("Один из последних просмотренных фильмов?");
-  let mark = prompt("На сколько оцените его?");
-  personalMovieDB.movies[namemv] = mark;
-}
+const a = prompt("Один из последних просмотренных фильмов?"),
+  b = prompt("На сколько оцените его?"),
+  c = prompt("Один из последних просмотренных фильмов?"),
+  d = prompt("На сколько оцените его?");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB.movies);
