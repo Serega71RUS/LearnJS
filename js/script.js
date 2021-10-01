61be8bbe92367835078913831675f6b41141f6d8
Serega71RUS <serega71rus@gmail.com>
2021-10-01 10:29:47 +0300

    my answer in task#2 015

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
 //confirm
 //prompt
 //= == ===
+//for, while(условие)[действия,], do[действия]while(условие)
 const numberOffilms = prompt("Сколько фильмов вы уже посмотрели?");
 const personalMovieDB = {
   count: numberOffilms,
@@ -13,9 +14,39 @@ const personalMovieDB = {
 };
 
 for(let i = 0; i < 2; i++){
-  const namemv = prompt("Один из последних просмотренных фильмов?");
-  const mark = prompt("На сколько оцените его?");
+  let namemv = "", 
+    mark = "";
+  while(namemv.length == 0 || namemv.length > 50){
+    namemv = prompt("Один из последних просмотренных фильмов?");
+  }
+  while(mark.length == 0){
+    mark = prompt("На сколько оцените его?");
+  }
   personalMovieDB.movies[namemv] = mark;
+}
+
+let i = 0;
+while(i < 2){
+  //.......
+  i++;
+}
+
+i = 0;
+do {
+  //.......
+  i++;
+}while(i < 2);
+
+if(personalMovieDB.count < 10){
+  console.log(`Просмотрено довольно мало фильмов`);
+}
+else{
+  if(personalMovieDB.count > 10 && personalMovieDB.count < 30){
+    console.log(`Вы классный зритель`);
+  }
+  else{
+    console.log(`Вы киноман`);
+  }
 }
 
 console.log(personalMovieDB);
